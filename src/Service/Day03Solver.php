45c3ebcd36942b09b8450d986c29c49a348c3d6b
Xeres <xeres666@googlemail.com>
2019-12-03 22:17:26 +0100

    [day3][part2] solved

--- src/Service/Day03Solver.php
+++ src/Service/Day03Solver.php
@@ -25,6 +25,95 @@ class Day03Solver extends AbstractDaySolver
 
     public function part2()
     {
+        $cables = $this->getInputFormated();
+        $grid = $this->layoutCables($cables);
+        $intersections = $this->getIntersections($grid);
+        $intersectionLengths = $this->getIntersectionLengths($cables, $intersections);
+        $this->logger->info("intersectionLengths", $intersectionLengths);
+
+        $cable1 = $intersectionLengths[0];
+        $cable2 = $intersectionLengths[1];
+        $minLength = -1;
+        foreach ($cable1 as $c1) {
+            foreach ($cable2 as $c2) {
+                if ($c1['x'] == $c2['x'] && $c1['y'] == $c2['y']) {
+                    $l = $c1['length'] + $c2['length'];
+                    $this->logger->info("L: $l");
+                    if ($minLength === -1 || $l < $minLength) {
+                        $minLength = $l;
+                    }
+                }
+            }
+        }
+        return $minLength;
+    }
+
+    protected function getIntersectionLengths($cables, $intersections)
+    {
+        $intersectionLengths = [];
+        $cableNr = 0;
+        foreach ($cables as $cable) {
+            $intersectionLengths[$cableNr] = [];
+            $x = 0;
+            $y = 0;
+            $steps = 0;
+            foreach ($cable as $direction) {
+                $dir = $direction['dir'];
+                $length = $direction['length'];
+                $dx = 0;
+                $dy = 0;
+                switch ($dir) {
+                    case 'U':
+                        $dx = 0;
+                        $dy = -1;
+                        break;
+                    case 'D':
+                        $dx = 0;
+                        $dy = 1;
+                        break;
+                    case 'L':
+                        $dx = -1;
+                        $dy = 0;
+                        break;
+                    case 'R':
+                        $dx = 1;
+                        $dy = 0;
+                        break;
+                }
+                for ($i = 0; $i < $length; $i++) {
+                    $x += $dx;
+                    $y += $dy;
+                    $steps += 1;
+
+                    foreach ($intersections as $intersection) {
+                        if ($intersection['x'] == $x && $intersection['y'] == $y) {
+                            $intersectionLengths[$cableNr][] = [
+                                'x' => $x,
+                                'y' => $y,
+                                'length' => $steps,
+                            ];
+                        }
+                    }
+                }
+            }
+            $cableNr += 1;
+        }
+        return $intersectionLengths;
+    }
+
+    protected function getIntersections($grid)
+    {
+        $intersections = [];
+        foreach ($grid as $position => $cell) {
+            if ($cell == 3) {
+                $p = explode('|', $position);
+                $intersections[] = [
+                    'x' => (int)$p[0],
+                    'y' => (int)$p[1],
+                ];
+            }
+        }
+        return $intersections;
     }
 
     protected function layoutCables($cables)
