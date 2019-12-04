32149cb696e2becf34fa2c6e4da010ca19eb1230
Xeres <xeres666@googlemail.com>
2019-12-04 21:22:01 +0100

    [general] add getInputFormated method to solver template

--- src/Maker/templates/daysolver.tpl.php
+++ src/Maker/templates/daysolver.tpl.php
@@ -11,4 +11,12 @@ class Day<?= $n ?>Solver extends AbstractDaySolver
     public function part2()
     {
     }
+
+    /**
+    * @return array
+    */
+    protected function getInputFormated()
+    {
+        return parent::getInputFormated();
+    }
 }
