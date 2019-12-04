6913a3e44262af067ec077bd5d850a3273d17b1e
Xeres <xeres666@googlemail.com>
2019-12-04 21:18:58 +0100

    [day4][part2] solved

--- src/Service/Day04Solver.php
+++ src/Service/Day04Solver.php
@@ -20,6 +20,44 @@ class Day04Solver extends AbstractDaySolver
 
     public function part2()
     {
+        $range = $this->getInputFormated();
+
+        $validPasswordCount = 0;
+        for ($i = $range[0]; $i <= $range[1]; $i++) {
+            if ($this->checkValidity2($i)) {
+                $validPasswordCount += 1;
+            }
+        }
+
+        return $validPasswordCount;
+    }
+
+    protected function checkValidity2($number)
+    {
+        $digits = str_split($number);
+        $repeatCounter = [];
+        $lastDigit = null;
+        foreach ($digits as $digit) {
+            if (!is_null($lastDigit)) {
+                if ($lastDigit == $digit) {
+                    if (!isset($repeatCounter[$digit])) {
+                        $repeatCounter[$digit] = 0;
+                    }
+                    $repeatCounter[$digit] += 1;
+                }
+                if ($lastDigit > $digit) {
+                    return false;
+                }
+            }
+            $lastDigit = $digit;
+        }
+
+        foreach ($repeatCounter as $digit => $count) {
+            if ($count == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     protected function checkValidity($number)
