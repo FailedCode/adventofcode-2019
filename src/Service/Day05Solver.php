2e17eafe0757a8c1fda7fdb6a5bbe9f46a860cb2
Xeres <xeres666@googlemail.com>
2019-12-10 00:05:22 +0100

    [day5][part1] solved

---
+++ src/Service/Day05Solver.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+class Day05Solver extends AbstractDaySolver
+{
+    public function part1()
+    {
+        $program = $this->getInputFormated();
+        $computer = new IntcodeComputer();
+        $computer->setCode($program);
+        $computer->addInput(1);
+        $computer->runProgram();
+        $output = $computer->getOutput();
+        return array_pop($output);
+    }
+
+    public function part2()
+    {
+    }
+
+    /**
+    * @return array
+    */
+    protected function getInputFormated()
+    {
+        $input = $this->puzzle->getInput();
+        return array_map(function ($value){ return (int)$value; }, explode(",", $input));
+    }
+}
