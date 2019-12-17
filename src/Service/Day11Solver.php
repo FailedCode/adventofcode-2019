83957601e03f302d50bfb4a4d8fe8e60d9420120
Xeres <xeres666@googlemail.com>
2019-12-17 15:58:21 +0100

    [day11][part2]

--- src/Service/Day11Solver.php
+++ src/Service/Day11Solver.php
@@ -52,5 +52,57 @@ class Day11Solver extends AbstractDaySolver
 
     public function part2()
     {
+        $program = $this->getInputIntcode();
+        $computer = new IntcodeComputer();
+        $computer->setCode($program);
+
+        // 0 = up, 1 = right, 2 = down, 3 = left
+        $direction = 0;
+        $x = 0;
+        $y = 0;
+        $xMax = 0;
+        $yMax = 0;
+        $grid = [];
+        $grid[$x][$y] = 1;
+        while (!$computer->hasHaltet()) {
+            $currentPanel = $grid[$x][$y] ?? 0;
+            $computer->addInput($currentPanel);
+            $grid[$x][$y] = $computer->runProgram(true);
+            $turn = $computer->runProgram(true);
+
+            if ($turn) {
+                $direction = ($direction + 1) % 4;
+            } else {
+                $direction = ($direction + 3) % 4;
+            }
+            switch ($direction) {
+                case 0:
+                    $y -= 1;
+                    break;
+                case 1:
+                    $x += 1;
+                    break;
+                case 2:
+                    $y += 1;
+                    break;
+                case 3:
+                    $x -= 1;
+                    break;
+            }
+
+
+            $xMax = max($xMax, $x);
+            $yMax = max($yMax, $y);
+        }
+
+        $text = '';
+        for ($y = 0; $y <= $yMax; $y++) {
+            for ($x = 0; $x <= $xMax; $x++) {
+                $v = $grid[$x][$y] ?? 0;
+                $text .= $v ? '#' : ' ';
+            }
+            $text .= "\n";
+        }
+        return "<pre>$text</pre>";
     }
 }
