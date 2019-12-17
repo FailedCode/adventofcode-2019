9e0bd0f4b25ad24b2a2539618e2ee6496efd6052
Xeres <xeres666@googlemail.com>
2019-12-17 16:42:12 +0100

    [day8][part1]

---
+++ src/Service/Day08Solver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Service;
+
+class Day08Solver extends AbstractDaySolver
+{
+    public function part1()
+    {
+        $imageData = $this->getInputFormated();
+        $width = 25;
+        $height = 6;
+        $dataCount = count($imageData);
+        $layers = $dataCount / ($width * $height);
+
+        $this->logger->info("$dataCount => $layers Layers");
+
+        $image = [];
+        $minZeroLayer = 0;
+        $minZeros = 9999;
+        for ($l = 0; $l < $layers; $l++) {
+            $zerosInLayer = 0;
+            for ($y = 0; $y < $height; $y++) {
+                for ($x = 0; $x < $width; $x++) {
+                    $p = $y * $width + $x + $width * $height * $l;
+                    $v = $imageData[$p];
+                    if ($v == 0) {
+                        $zerosInLayer += 1;
+                    }
+                    $image[$l][$y][$x] = $v;
+                }
+            }
+            if ($zerosInLayer < $minZeros) {
+                $minZeros = $zerosInLayer;
+                $minZeroLayer = $l;
+            }
+        }
+
+        $ones = 0;
+        $twoes = 0;
+        for ($y = 0; $y < $height; $y++) {
+            for ($x = 0; $x < $width; $x++) {
+                if ($image[$minZeroLayer][$y][$x] == 1) {
+                    $ones += 1;
+                }
+                if ($image[$minZeroLayer][$y][$x] == 2) {
+                    $twoes += 1;
+                }
+            }
+        }
+
+        return $ones * $twoes;
+    }
+
+    public function part2()
+    {
+    }
+
+    /**
+    * @return array
+    */
+    protected function getInputFormated()
+    {
+        $data = parent::getInputFormated();
+        return str_split(implode('', $data));
+    }
+}
